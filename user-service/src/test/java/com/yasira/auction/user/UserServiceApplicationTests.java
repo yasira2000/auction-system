3fa36f0cca716bd4df55a083aa849d1cae4148f0
yasira2000 <yasirapunsith@gmail.com>
2025-01-12 08:18:52 +0530

    Initial commit

---
+++ user-service/src/test/java/com/yasira/auction/user/UserServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.yasira.auction.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
