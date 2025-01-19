be05abc50de8f3be89a1597297d4622c0801047c
yasira2000 <yasirapunsith@gmail.com>
2025-01-19 21:55:40 +0530

    create getAllAuctions endpoint

--- auction-management-service/src/main/java/com/yasira/auction/auctionmanagementservice/controller/AuctionController.java
+++ auction-management-service/src/main/java/com/yasira/auction/auctionmanagementservice/controller/AuctionController.java
@@ -7,7 +7,6 @@ import com.yasira.auction.auctionmanagementservice.dto.AuctionResponseDTO;
 import com.yasira.auction.auctionmanagementservice.service.AuctionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +21,7 @@ public class AuctionController {
     private final AuctionService auctionService;
 
     @PostMapping
-    public ResponseEntity<?> createAuction(@Valid @RequestBody AuctionRequestDTO auctionRequestDTO) {
+    public ResponseEntity<ApiResponse<AuctionResponseDTO>> createAuction(@Valid @RequestBody AuctionRequestDTO auctionRequestDTO) {
         AuctionResponseDTO auctionResponseDTO = auctionService.createAuction(auctionRequestDTO);
         URI location = URI.create("/api/v1/products/" + auctionResponseDTO.auctionId());
         return ResponseEntity.created(location)
@@ -34,12 +33,12 @@ public class AuctionController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllAuctions() {
+    public ResponseEntity<ApiResponse<List<AuctionResponseDTO>>> getAllAuctions() {
         List<AuctionResponseDTO> auctionsList = auctionService.getAllAuctions();
         return ResponseEntity
                 .ok(new ApiResponse<>(
                         true,
-                        "Auction created successfully",
+                        "Auctions List retrieved successfully",
                         auctionsList
                 ));
     }
